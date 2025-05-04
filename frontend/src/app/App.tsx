ff0fd9628ed45a894a5a91350a2bb53034ad404d
MarcoDecco <marcodecco80@gmail.com>
2025-05-04 08:31:01 -0300

    Arrumando esquema de pastas

--- frontend/src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-import Routes from '../routes';
-
-// direciona o aplicativo para o arquivo index.tsx dentro da pasta routes
-export default function App() {
-  return (
-    <Routes />
-  );
-}
--- frontend/src/styles/fontFamily.ts
+++
@@ -1,5 +0,0 @@
-export const fontFamily = {
-    regular: "Roboto_400Regular",
-    medium: "Roboto_500Medium",
-    bold: "Roboto_700Bold",
-}
