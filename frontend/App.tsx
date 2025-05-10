b5870ccb8843b739208c3acec53cc34a39270dca
br3no4k <brenoakuhn@gmail.com>
2025-05-10 12:33:53 -0300

    Alterações nas teas de autenticação

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,22 @@
 import './global.css';
 import NavigationContext from '~/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContext />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <NavigationContext />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaProvider>
   );
 }
