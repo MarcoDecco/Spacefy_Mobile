66601851c65a83e010164b7a7e857668f9c46f3d
Yuri <yuque7@gmail.com>
2025-06-13 14:56:17 -0300

    Funcionando

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,40 +1,8 @@
-import React, { useEffect } from 'react';
-import * as SQLite from 'expo-sqlite';
 import NavigationContext from '~/navigation';
 import { ThemeProvider } from './src/contexts/ThemeContext';
 import { AuthProvider } from './src/contexts/AuthContext';
 
-const db = SQLite.openDatabase('app.db');
-
 export default function App() {
-useEffect(() => {
-    db.transaction(tx => {
-      // Tabela de login
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS login (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          user_id TEXT,
-          token TEXT
-        );`
-      );
-      // Tabela de favoritos
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS favoritos (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          space_id TEXT,
-          user_id TEXT
-        );`
-      );
-      // Tabela de alugados
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS alugados (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          space_id TEXT,
-          user_id TEXT
-        );`
-      );
-    });
-  }, []);
 
   return (
     <ThemeProvider>
